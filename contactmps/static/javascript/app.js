7012b0ed81c2f15ca604d18a10f14caebf320815
JD Bothma <jbothma@gmail.com>
2017-06-06 17:20:15 +0200

    Fix button enable in prod

--- contactmps/static/javascript/app.js
+++
@@ -1,7 +0,0 @@
-var gReCaptchaValidated = function() {
-  $("input[type=submit]").removeAttr('disabled');
-};
-
-var gReCaptchaExpired = function() {
-  $("input[type=submit]").attr('disabled','disabled');
-};
