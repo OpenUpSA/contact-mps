d9fa51388b8188491185c43b59eb4cdc52512698
JD Bothma <jbothma@gmail.com>
2017-06-06 13:06:37 +0200

    Put recaptcha inside head

--- contactmps/static/javascript/embed.js
+++ contactmps/static/javascript/embed.js
@@ -5,5 +5,7 @@ if (document.location.hostname == "localhost") {
 }
 document.write('<div id="contactmps-embed-parent"></div>');
 document.write('<script type="text/javascript" src="' + baseurl + '/static/javascript/pym.v1.min.js"></script>');
-// Forward slash is necessary for HTTPS because django redirect to url/ uses HTTP
-document.write("<script>var pymParent = new pym.Parent('contactmps-embed-parent', '" + baseurl + "/campaign/noconfidence/', {});</script>");
+
+if (typeof pym === 'undefined') {
+  document.write("<script>var pymParent = new pym.Parent('contactmps-embed-parent', '" + baseurl + "/campaign/noconfidence/', {});</script>");
+}
