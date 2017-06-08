ab2229b69cdf8357c652b2286b433390b29959a6
JD Bothma <jbothma@gmail.com>
2017-06-08 13:02:29 +0200

    Try Facebook SDK share button again because iframe wasn't working on mobile
    Also tweak share button data a bit

--- contactmps/settings.py
+++ contactmps/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-BASE_URL = os.environ.get('BASE_URL', 'http://localhost:8000/')
+BASE_URL = os.environ.get('BASE_URL', 'http://localhost:8000')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
