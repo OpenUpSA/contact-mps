7001a99ac6e7341b900472cb72eb92385765bf6d
JD Bothma <jbothma@gmail.com>
2017-06-25 18:51:04 +0200

    Fix background - abs url for injected code

--- contactmps/static/javascript/embed.js
+++ contactmps/static/javascript/embed.js
@@ -17,7 +17,7 @@ if (agent.includes("mobile") && agent.includes("android")) {
     });
   });
   // Don't initialise pymParent! we iframe it ourselves!
-  document.write('<div id="contactmps-embed-parent" style="height: 2500px; background: url(\'/static/images/background.svg\'); background-repeat: no-repeat"><iframe src="' + baseurl + '/campaign/newsmedia/" width="100%" scrolling="no" marginheight="0" frameborder="0" height="2500px" style="height: 2500px">Loading...</iframe></div>');
+  document.write('<div id="contactmps-embed-parent" style="height: 2500px; background: url(https://noconfidencevote.openup.org.za/static/images/background.svg); background-repeat: no-repeat"><iframe src="' + baseurl + '/campaign/newsmedia/" width="100%" scrolling="no" marginheight="0" frameborder="0" height="2500px" style="height: 2500px">Loading...</iframe></div>');
 } else {
   document.write('<div id="contactmps-embed-parent"></div>');
   document.write('<script type="text/javascript" src="' + baseurl + '/static/javascript/pym.v1.min.js" crossorigin="anonymous" async defer onload="initContactMPsPymParent()"></script>');
