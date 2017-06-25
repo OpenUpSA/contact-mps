284937a19ce96ac4eef1d9b5354c13b720bdd03b
JD Bothma <jbothma@gmail.com>
2017-06-25 18:34:59 +0200

    Add background image to help users with slow loading and static height

--- contactmps/static/javascript/embed.js
+++ contactmps/static/javascript/embed.js
@@ -15,10 +15,9 @@ if (agent.includes("mobile") && agent.includes("android")) {
     window.addEventListener('error', function(e) {
       ga('send', 'event', 'JavaScript Error Parent', e.filename + ':  ' + e.lineno, e.message);
     });
-
   });
   // Don't initialise pymParent! we iframe it ourselves!
-  document.write('<div id="contactmps-embed-parent" style="height: 2500px"><iframe src="' + baseurl + '/campaign/newsmedia/" width="100%" scrolling="no" marginheight="0" frameborder="0" height="2500px" style="height: 2500px">Loading...</iframe></div>');
+  document.write('<div id="contactmps-embed-parent" style="height: 2500px; background: url(\'/static/images/background.svg\'); background-repeat: no-repeat"><iframe src="' + baseurl + '/campaign/newsmedia/" width="100%" scrolling="no" marginheight="0" frameborder="0" height="2500px" style="height: 2500px">Loading...</iframe></div>');
 } else {
   document.write('<div id="contactmps-embed-parent"></div>');
   document.write('<script type="text/javascript" src="' + baseurl + '/static/javascript/pym.v1.min.js" crossorigin="anonymous" async defer onload="initContactMPsPymParent()"></script>');
