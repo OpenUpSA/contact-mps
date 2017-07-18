e229827be129976c8265d8903be074dda1376690
Guus Hoekman <mail@guushoekman.com>
2017-07-18 13:27:36 +0200

    days remaining

--- contactmps/static/javascript/secretballot.js
+++ contactmps/static/javascript/secretballot.js
@@ -1,1 +1,10 @@
 console.log("secretballot");
+
+var daysRemaining=(function(){ 
+    var oneDay = 24*60*60*1000;
+    var firstDate = new Date();
+    var secondDate = new Date(2017, 6, 25, 23, 59);
+    return Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+})();
+
+console.log(daysRemaining);
