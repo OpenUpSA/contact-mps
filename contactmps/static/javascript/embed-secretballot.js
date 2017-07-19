552ed75e35ab42d035d205a33f910ac627bd7443
JD Bothma <jbothma@gmail.com>
2017-07-19 18:03:07 +0200

    Add error reporting to the parent

--- contactmps/static/javascript/embed-secretballot.js
+++ contactmps/static/javascript/embed-secretballot.js
@@ -1,3 +1,27 @@
+// addEventListener only available in later chrome versions
+window.addEventListener("load",function(){
+  function doError(msg, url, ln) {
+    var strValues = "errMsg=" + escape(msg);
+    strValues += "&errLine=" + ln;
+    strValues += "&queryString=" + escape(location.search);
+    strValues += "&Url=" + escape(location.pathname);
+    strValues += "&HTTPRef=" + escape(document.referrer);
+
+    if (typeof XMLHttpRequest != "object") {
+      function XMLHttpRequest() {
+        return new ActiveXObject("Microsoft.XMLHTTP");
+      }
+    }
+    var objSave = new XMLHttpRequest();
+    objSave.open("GET", baseurl + "/errorSave/?" + strValues, false);
+    objSave.send("");
+  }
+  window.addEventListener('error', function(e) {
+    try {    window.onerror = doError;}catch(er) {}
+  });
+});
+
+
 if (document.location.hostname == "localhost") {
     var baseurl = "";
 } else {
