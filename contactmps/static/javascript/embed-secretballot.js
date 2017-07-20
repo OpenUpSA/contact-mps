908347ce15eb5a34655c06a7abff8a9db8a1be63
JD Bothma <jbothma@gmail.com>
2017-07-20 16:07:40 +0200

    Indicate cross-origin script is allowed

--- contactmps/static/javascript/embed-secretballot.js
+++ contactmps/static/javascript/embed-secretballot.js
@@ -32,7 +32,7 @@ if (isAndroidApp) {
 }
 
 document.write('<div id="contactmps-embed-parent"></div>');
-document.write('<script type="text/javascript" src="' + baseurl + '/static/javascript/pym.v1.min.js"></script>');
+document.write('<script type="text/javascript" src="' + baseurl + '/static/javascript/pym.v1.min.js" crossorigin="anonymous"></script>');
 
 document.write("<script>var pymParent = new pym.Parent('contactmps-embed-parent', '" + baseurl + "/campaign/secretballot/', {});</script>");
 
