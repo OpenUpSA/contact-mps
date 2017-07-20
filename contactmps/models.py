e170611a9704b7bb97b42d53e0897b07782f1b38
JD Bothma <jbothma@gmail.com>
2017-07-20 17:27:31 +0200

    live emails again

--- contactmps/models.py
+++ contactmps/models.py
@@ -140,7 +140,7 @@ class Email(models.Model):
         if not EMAIL_RE.match(self.from_email):
             self.from_email = "noreply@openup.org.za"
         sender = "%s <%s>" % (self.from_name, self.from_email)
-        recipients = ["%s <jd@openup.org.za>" % self.to_person.name.strip() for c in self.to_person.contactdetails.filter(type='email').all()]
+        recipients = ["%s <%s>" % (self.to_person.name.strip(), c.value.strip()) for c in self.to_person.contactdetails.filter(type='email').all()]
         if settings.SEND_EMAILS:
             log.info("Sending email to %s from %s" % (recipients, self.from_email))
 
